98f1a113fc9750fa162a9ba48a993604f2a86ced
Fernanda Chacon <fernandadmz1995@gmail.com>
2026-02-23 10:49:18 -0500

    Update dim_date.sql

--- sql/schema/dim_date.sql
+++ sql/schema/dim_date.sql
@@ -2,7 +2,7 @@
 -- Purpose: Date dimension derived from pickup/dropoff timestamps.
 -- Provides common reporting attributes (month, quarter, weekend flag) for time-based aggregations.
 
-CREATE TABLE dim_date (
+CREATE TABLE IF NOT EXISTS dim_date (
   date_key DATE PRIMARY KEY,
   year SMALLINT NOT NULL,
   month SMALLINT NOT NULL,
