f57a91c41f8414f33b932778130f7ffaf3199b9b
Fernanda Chacon <fernandadmz1995@gmail.com>
2026-02-22 23:07:13 -0500

    Update dim_date.sql

--- sql/schema/dim_date.sql
+++ sql/schema/dim_date.sql
@@ -1,8 +1,11 @@
 -- Dimension: Date
 CREATE TABLE dim_date (
-    date_key DATE PRIMARY KEY,
-    year INT,
-    month INT,
-    day INT,
-    day_of_week INT
+  date_key DATE PRIMARY KEY,
+  year SMALLINT NOT NULL,
+  month SMALLINT NOT NULL,
+  day SMALLINT NOT NULL,
+  day_of_week SMALLINT NOT NULL,
+  month_name VARCHAR(20),
+  quarter SMALLINT,
+  is_weekend BOOLEAN
 );
